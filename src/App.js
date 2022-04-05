c9b894199bbe76a885281c5b620fa1aba1045a15
Jhon Álvarez <jwalvarez.98@gmail.com>
2022-04-05 00:42:26 -0500

    Styling updated and Custom component added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import axios from "axios";
 import { Button } from "react-bootstrap";
 
 import NoRecipes from "./components/NoRecipes";
+import Recipe from "./components/Recipe";
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  // Add recipe to the state
   const getRequest = () => {
     axios
       .get(
@@ -29,33 +33,59 @@ const App = () => {
       });
   };
 
-  useEffect(() => {
-    updateTotal();
-    // document.title = `$ ${updateTotal()} usd`;
-  });
-
-  const updateTotal = () => {
-    let total = 0;
-    recipes.forEach((recipe) => {
-      total += recipe.price;
-    });
-    console.log(total);
-    return total;
+  //Delete recipe with provided id
+  const deleteRecipe = (recipe) => {
+    console.log(recipes);
+    recipes.splice(recipes.indexOf(recipe), 1);
+    setRecipes([...recipes]);
   };
 
-  const [recipes, setRecipes] = useState([]);
+  // update variables
+  const [menuInformation, setMenuInformation] = useState({
+    total: 0,
+    avgPreparationTime: 0,
+    avgHS: 0,
+  });
+
+  // ? Why we write this line?
+  const { total, avgPreparationTime, avgHS } = menuInformation;
+
+  const updateMenuInfo = () => {
+    var total = 0;
+    var avgPreparationTime = 0;
+    var avgHS = 0;
+    recipes.forEach((recipe) => {
+      total += recipe.price;
+      avgPreparationTime += recipe.time;
+      console.log(recipe.healthScore);
+      avgHS += parseFloat(recipe.healthScore);
+    });
+
+    setMenuInformation({
+      total: total,
+      avgPreparationTime: avgPreparationTime / recipes.length,
+      avgHS: parseFloat(avgHS) / recipes.length,
+    });
+  };
+
+  useEffect(() => {
+    console.log("Update Price!!!");
+    updateMenuInfo();
+    // updateTotal();
+    // document.title = `$ ${updateTotal()} usd`;
+  }, [recipes]);
 
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <nav className="d-flex justify-content-between navbar navbar-expand-lg navbar-light bg-light px-4 mb-4">
-        <a className="navbar-brand" href="#">
-          {/* <img
+        <a className="navbar-brand mb-2" href="#">
+          <img
             src="https://getbootstrap.com/docs/4.1/assets/brand/bootstrap-solid.svg"
             width="30"
             height="30"
-            className="d-inline-block align-top"
+            className="d-inline-block align-top me-2"
             alt=""
-          /> */}
+          />
           Menú Alkemy
         </a>
         <form className="d-flex w-100">
@@ -74,70 +104,50 @@ const App = () => {
           </Button>
         </form>
       </nav>
-      <div className="mx-4">
+      {recipes.length == 0 ? (
+        <></>
+      ) : (
+        <div className="mx-4 d-flex justify-content-start justify-content-between">
+          <span className="d-inline">
+            TOTAL:
+            <small className="rounded-3 bg-primary bg-gradient text-light px-2 py-1 mx-2">
+              {Math.ceil(total)} USD
+            </small>
+          </span>
+          <span className="d-inline">
+            Promedio HS:
+            <small className="rounded-3 bg-primary bg-gradient text-light px-2 py-1 mx-2">
+              {Math.ceil(avgPreparationTime)} min
+            </small>
+          </span>
+          <span className="d-inline">
+            Promedio HS:
+            <small className="rounded-3 bg-primary bg-gradient text-light px-2 py-1 mx-2">
+              {Math.ceil(avgHS)} HS
+            </small>
+          </span>
+        </div>
+      )}
+      <div className="x-4">
         {recipes.length == 0 ? (
           <NoRecipes />
         ) : (
           <ul className="p-0">
             {recipes.map((recipe) => (
-              <li className="list-unstyled" key={recipe.id}>
-                <div className="card border-primary mb-3">
-                  <div className="row g-0">
-                    <div className="col-md-4">
-                      <img
-                        src={recipe.img}
-                        className="w-100 img-fluid rounded-top"
-                        alt={recipe.img}
-                      />
-                    </div>
-                    <div className="col-md-8">
-                      <div className="card-body">
-                        <div className="row">
-                          <div className="col">
-                            <h5 className="card-title">
-                              <strong>{recipe.title}</strong>
-                            </h5>
-                          </div>
-
-                          <div className="col-2 d-flex justify-content-end">
-                            <i className="bi bi-trash-fill" role="button"></i>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col">
-                            {recipe.props.map((prop) => (
-                              <small
-                                key={prop}
-                                className="d-inline rounded-3 bg-dark text-light px-2 py-1 my-4 me-2"
-                              >
-                                {prop}
-                              </small>
-                            ))}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="card-footer">
-                      <div className="row">
-                        <small className="col text-muted">
-                          Duración: {recipe.time} min
-                          <br />
-                          {recipe.healthScore} Health Score
-                        </small>
-                        <h3 className="col card-text d-flex justify-content-end my-auto">
-                          <strong>{recipe.price} USD</strong>
-                        </h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+              <li className="list-unstyled grid" key={recipe.id}>
+                <Recipe deleteRecipe={deleteRecipe} recipe={recipe} />
               </li>
             ))}
           </ul>
         )}
-        <footer className="bg-dark p-3"></footer>
       </div>
-    </>
+
+      <footer className="mt-auto bg-dark p-3 d-flex justify-content-center">
+        <small className="text-muted text-light">
+          Yeah boy, this is my footer
+        </small>
+      </footer>
+    </div>
   );
 };
 
